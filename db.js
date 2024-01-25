84b0a7c6a9c33706dfd458b07f75e0b0ef31ba28
gabriel-eustaquio <gabriel.s.eustaquio@gmail.com>
2024-01-25 20:32:33 -0300

    backend

---
+++ db.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const { MONGO_URL } = process.env;
+
+async function connect() {
+  const url = MONGO_URL;
+  const client = new MongoClient(url, options);
+  global.connection = await client.connect();
+  global.db = global.connection.db("Webbank");
+  global.collection = global.db.collection("Customers");
+}
+
+connect();
+
+function insertCustomer(dataReq) {
+  return global.collection.insertOne(dataReq);
+}
+
+async function findCustomer(dataReq) {
+  const data = await global.collection.find({ $and: [dataReq] }).toArray();
+  return data;
+}
+
+module.exports = {
+  insertCustomer,
+  findCustomer,
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./db");
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/insertCustomer", (req, res) => {
+  db.insertCustomer(req.body);
+  res.sendStatus(200);
+});
+
+app.post("/findCustomer", async (req, res) => {
+  const data = await db.findCustomer(req.body);
+  res.send(data);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
